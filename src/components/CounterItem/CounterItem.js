8f51783cee7c22d874af8ce868696bb5b4e05beb
Ondrej Zvara <ondrej.zvara@cleverfarm.cz>
2018-06-16 01:29:28 +0200

    Countdown logic

--- src/components/CounterItem/CounterItem.js
+++ src/components/CounterItem/CounterItem.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./CounterItem.css";
 
-const CounterItem = () => {
-  return <div className="CounterItem" />;
+const CounterItem = props => {
+  return (
+    <div className="CounterItem">
+      <div className="CounterItem-value">{props.value}</div>
+      <div className="CounterItem-desc">{props.desc}</div>
+    </div>
+  );
 };
 
 export default CounterItem;
--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,19 +1,67 @@
+import moment from "moment";
 import React, { Component } from "react";
 import "./Counter.css";
 import CounterItem from "./../../components/CounterItem/CounterItem";
 
 class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      countdown: {
+        days: null,
+        hours: null,
+        minutes: null,
+        seconds: null
+      }
+    };
+  }
+
+  componentDidMount() {
+    const blackFriday = this.resolveBlackFridayDate(moment().year());
+    setInterval(() => {
+      const duration = moment.duration(blackFriday.diff(moment()));
+      this.setState({
+        countdown: {
+          days: Math.trunc(duration.asDays()),
+          hours: ("0" + duration.hours()).slice(-2),
+          minutes: ("0" + duration.minutes()).slice(-2),
+          seconds: ("0" + duration.seconds()).slice(-2)
+        }
+      });
+    }, 1000);
+  }
+
+  resolveBlackFridayDate(year) {
+    // BF is always after Thanksgiving
+    // TG is always fourth Wednesday in November
+    const wednesdayIndex = 4;
+
+    const november = moment([year, 10]);
+    const firstDayIndex = november.isoWeekday();
+
+    let firstWednesday = undefined;
+    if (firstDayIndex <= wednesdayIndex) {
+      firstWednesday = november.isoWeekday(wednesdayIndex);
+    } else {
+      firstWednesday = november.add(1, "weeks").isoWeekday(wednesdayIndex);
+    }
+
+    return firstWednesday.add(3, "w").add(1, "d");
+  }
+
   render() {
+    const { countdown } = this.state;
+
     return (
       <div className="Counter">
         <header className="Counter-header">
           <p>To get started</p>
         </header>
         <main className="Counter-items">
-          <CounterItem />
-          <CounterItem />
-          <CounterItem />
-          <CounterItem />
+          {Object.keys(countdown).map(key => {
+            return <CounterItem value={countdown[key]} desc={key} />;
+          })}
         </main>
       </div>
     );
